39982a48a2c241d8abd241e7b50b256d706f0411
Caique Rodrigues <caiquepeixoto1@gmail.com>
2019-06-21 21:10:18 -0300

    Add files to enable deployment to production environment.

---
+++ production-environment.tf
@@ -0,0 +1,35 @@
+resource "google_project_service" "cloudresourcemanager_googleapis_com" {
+    service = "cloudresourcemanager.googleapis.com"
+}
+
+resource "google_project_service" "container_googleapis_com" {
+    service = "container.googleapis.com"
+    depends_on = [
+        "google_project_service.cloudresourcemanager_googleapis_com"
+    ]
+}
+
+resource "google_container_cluster" "production_cluster" {
+    name = "production-cluster"
+    location = "us-east1-b"
+    initial_node_count = 1
+
+    node_locations = [
+        "us-east1-c"
+    ]
+
+    node_config {
+        oauth_scopes = [
+            "https://www.googleapis.com/auth/compute",
+            "https://www.googleapis.com/auth/devstorage.read_only",
+            "https://www.googleapis.com/auth/logging.write",
+            "https://www.googleapis.com/auth/monitoring",
+        ]
+    }
+
+    depends_on = [
+        "google_project_service.container_googleapis_com"
+    ]
+}
+
+
